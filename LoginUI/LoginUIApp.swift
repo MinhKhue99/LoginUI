2287f7ca809e4f419c999b9ca1d8b04871a35c0d
KhuePM <minhkhuepham99@gmail.com>
2023-09-27 23:35:39 +0700

    Initial Commit

---
+++ LoginUI/LoginUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginUIApp.swift
+//  LoginUI
+//
+//  Created by Phạm Minh Khuê on 27/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
